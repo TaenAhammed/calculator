64012881cc2b45b81a7b4c19c899bed81db36143
Taen Ahammed <taenahammed7@gmail.com>
2019-07-12 23:23:53 +0600

    Updates

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-const state = { operationCount: 0, evalCount: 0 };
-let history = "",
-    input = "",
-    result = 0,
-    temp = 0;
-const calculate = input => {
-    result = eval(input);
-    temp = result;
-    state.evalCount++;
-    return result;
+let num1, num2, temp, result, history;
+init();
+function init() {
+    num1 = "";
+    num2 = "";
+    temp = "";
+    history = "";
+    result = 0;
+}
+class Calculator {
+    constructor(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    addition() {
+        return this.num1 + this.num2;
+    }
+    subtraction() {
+        return this.num1 - this.num2;
+    }
+    multiplication() {
+        return this.num1 * this.num2;
+    }
+    division() {
+        return this.num1 / this.num2;
+    }
+}
+
+const state = {
+    hasActiveOperation: false,
+    activeOperationCount: 0,
+    operation: ""
 };
+
+const stateCopy = { ...state };
+
+function calculate() {}
 
 function showHistory() {
     document.querySelector(".calculator_history").textContent = history;
@@ -23,33 +49,59 @@ for (calculatorInput of calculatorInputs) {
     calculatorInput.addEventListener("click", e => {
         let id = e.target.id;
         if (id === "plus" || id === "minus" || id === "mul" || id === "div") {
-            state.hasActiveOperation = true;
-            state.operationCount++;
+            state.hasActiveOperation = !state.hasActiveOperation;
+            state.activeOperationCount++;
+
+            stateCopy.operation = e.target.textContent;
         } else {
-            state.hasActiveOperation = false;
+            let number = e.target.textContent;
+            if (state.hasActiveOperation) {
+                num2 += number;
+                num2 = parseFloat(num2);
+            } else {
+                num1 += number;
+                num1 = parseFloat(num1);
+            }
+        }
+        if (state.activeOperationCount > 1) {
+            const cal = new Calculator(num1, num2);
+
+            if (state.operation === "+") {
+                result = cal.addition(num1, num2);
+                showResult();
+            }
+
+            if (state.operation === "-") {
+                result = cal.subtraction(num1, num2);
+                showResult();
+            }
+
+            if (state.operation === "*") {
+                result = cal.multiplication(num1, num2);
+                showResult();
+            }
+
+            if (state.operation === "/") {
+                result = cal.division(num1, num2);
+                showResult();
+            }
         }
 
-        if (state.evalCount > 1 && state.hasActiveOperation) {
-            input = `${temp}`;
-        }
+        state.operation = stateCopy.operation;
 
         history += e.target.textContent;
         showHistory();
-
-        input += e.target.textContent;
-        if (!state.hasActiveOperation) {
-            result = calculate(input);
-            showResult();
-        }
     });
 }
+
 document.getElementById("c").addEventListener("click", () => {
-    state.operationCount = 0;
-    state.evalCount = 0;
-    history = "";
-    input = "";
-    result = 0;
-    temp = 0;
-    showHistory();
+    state.hasActiveOperation = false;
+    state.activeOperationCount = 0;
+    state.operation = "";
+    stateCopy.operation = "";
+    init();
     showResult();
+    showHistory();
 });
+
+document.getElementById("equal").addEventListener("click", () => console.log("equal"));
